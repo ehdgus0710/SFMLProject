312ebfc3ba4012347cf8f51951d365d98e88d3d7
김동현 <ehdgus0710@naver.com>
2024-10-22 17:12:23 +0900

    game, sprite 오브젝트 추가 및 수정

--- SFMLProject/GameObject.cpp
+++ SFMLProject/GameObject.cpp
@@ -1,6 +1,53 @@
 #include "stdafx.h"
 #include "GameObject.h"
 
+
+GameObject::GameObject(const std::string& name)
+    : name(name)
+	, originPreset(Origins::MiddleCenter)
+{
+}
+
+GameObject::GameObject(const GameObject& other)
+	: name(other.name)
+	, position(other.position)
+	, rotation(other.rotation)
+	, scale(other.scale)
+	, origin(other.origin)
+	, originPreset(other.originPreset)
+	, active(other.active)
+
+
+{
+}
+
+void GameObject::SetOrigin(Origins preset)
+{
+	originPreset = preset;
+	origin = {};
+}
+
+void GameObject::Init()
+{
+}
+
+void GameObject::Awake()
+{
+}
+
+void GameObject::Start()
+{
+}
+
+void GameObject::Release()
+{
+}
+
+void GameObject::Reset()
+{
+}
+
+
 void GameObject::Update(const float& deltaTime)
 {
 }
@@ -13,15 +60,7 @@ void GameObject::LateUpdate(const float& deltaTime)
 {
 }
 
-void GameObject::Render(const sf::RenderWindow& renderWindow)
-{
-}
-
-void GameObject::Awake()
-{
-}
-
-void GameObject::Start()
+void GameObject::Render(sf::RenderWindow& renderWindow)
 {
 }
 
